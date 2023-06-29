ce3b1421dbc44aef0ec89c162c33ca0c2d315165
olamide jubril <olamidejubril68@gmail.com>
2023-06-29 16:55:36 +0100

    Writing the first unit test

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,36 @@
-FROM openjdk:11-jdk-slim
+# The base image is set to maven:3.8.3-openjdk-11-slim,
+# which includes Maven for building the application.
+FROM maven:3.8.3-openjdk-11-slim AS build
 
+# The working directory for the build stage is set to /app.
 WORKDIR /app
 
-# Copy the executable JAR file into the container
-COPY target/your-app.jar app.jar
+# The first stage, build, sets up the Maven build environment and
+# copies the project's pom.xml file. It then resolves the dependencies
+# using mvn dependency:go-offline to optimize subsequent builds.
+COPY pom.xml .
+RUN mvn dependency:go-offline
 
+# The src directory is copied into the build environment, and the
+# application is packaged using mvn package -DskipTests
+# (skipping tests for the Docker build).
+COPY src ./src
+RUN mvn package -DskipTests
+
+# The second stage starts with a new base image openjdk:11-jre-slim
+# to reduce the image size.
+FROM openjdk:11-jre-slim
+
+# The working directory is set to /app.
+WORKDIR /app
+
+# The JAR file built in the build stage is copied from the
+# previous stage using COPY --from=build.
+COPY --from=build /app/target/*.jar app.jar
+
+# Port 8080 is exposed to allow external access.
 EXPOSE 8080
 
+# The CMD instruction specifies the command to run the Spring Boot
+# application using the JAR file
 CMD ["java", "-jar", "app.jar"]
--- src/test/java/com/olamide/startup_progress_tracker/services/PhaseServiceImplTest.java
+++ src/test/java/com/olamide/startup_progress_tracker/services/PhaseServiceImplTest.java
@@ -1,2 +1,53 @@
-package com.olamide.startup_progress_tracker.services;public class PhaseServiceImplTest {
+package com.olamide.startup_progress_tracker.services;
+
+import com.olamide.startup_progress_tracker.DTO.PhaseDTO;
+import com.olamide.startup_progress_tracker.entities.Phase;
+import com.olamide.startup_progress_tracker.repositories.PhaseRepository;
+import com.olamide.startup_progress_tracker.repositories.TaskRepository;
+import com.olamide.startup_progress_tracker.services.Implementation.PhaseServiceImpl;
+import com.olamide.startup_progress_tracker.utils.PhaseSequenceGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class PhaseServiceImplTest {
+    private PhaseServiceImpl phaseService;
+
+    @Mock
+    private PhaseRepository phaseRepository;
+
+    @Mock
+    private PhaseSequenceGenerator phaseSequenceGenerator;
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        phaseService = new PhaseServiceImpl(phaseRepository, taskRepository, phaseSequenceGenerator);
+    }
+
+    @Test
+    void createPhase_ShouldReturnCreatedPhaseDTO(){
+        // Arrange
+        Phase phase = new Phase();
+        phase.setDone(false);
+        phase.setStage(1);
+        when(phaseSequenceGenerator.getNextStage()).thenReturn(1);
+        when(phaseRepository.save(phase)).thenReturn(phase);
+
+        // Act
+        PhaseDTO result = phaseService.createPhase(phase);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(phase.getStage(), result.getStage());
+        assertFalse(result.getDone());
+        verify(phaseRepository, times(1)).save(phase);
+    }
 }
