c41769afefb2bdae8bb307c5e28296e415f0f5e1
olamide jubril <olamidejubril68@gmail.com>
2023-06-29 08:40:07 +0100

    Final fixes on the authentication module

--- src/main/java/com/olamide/startup_progress_tracker/security/SecurityConfig.java
+++ src/main/java/com/olamide/startup_progress_tracker/security/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @AllArgsConstructor
 @Configuration
@@ -48,42 +51,39 @@ public class SecurityConfig {
         return http.build();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-//        var exceptionHandlerFilter = new ExceptionHandlerFilter(responseHandler);
-//        var authenticationFilter = new AuthenticationFilter(authenticationManager, responseHandler);
-//        authenticationFilter.setFilterProcessesUrl("/authenticate");
-//
-//        http
-//                .cors(Customizer.withDefaults())
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .addFilter(authenticationFilter)
-//                .authorizeHttpRequests(request -> request
-//                        .requestMatchers("/h2/**").permitAll()
-//                        .requestMatchers(HttpMethod.POST, SecurityConstants.AUTH_ENDPOINT).permitAll()
-//                        .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll()
-//
-//                        //.requestMatchers(SecurityConstants.AUTH_ENDPOINT, SecurityConstants.REGISTER_PATH).permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//        http
-//                .addFilterBefore(exceptionHandlerFilter, AuthenticationFilter.class)
-//                .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class);
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        var exceptionHandlerFilter = new ExceptionHandlerFilter(responseHandler);
+        var authenticationFilter = new AuthenticationFilter(authenticationManager, responseHandler);
+        authenticationFilter.setFilterProcessesUrl("/authenticate");
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        config.addAllowedOrigin("*");
-//        config.setAllowCredentials(true);
-//        source.registerCorsConfiguration("/**", config);
-//        return source;
-//    }
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilter(authenticationFilter)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/h2/**", SecurityConstants.AUTH_ENDPOINT, SecurityConstants.REGISTER_PATH)
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http
+                .addFilterBefore(exceptionHandlerFilter, AuthenticationFilter.class)
+                .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
